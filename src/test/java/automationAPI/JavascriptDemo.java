6ca6d64478fd1daf3a6869cbea3a63f9b807dfd1
Faraz Baig <baigbuilders.mbd@gmail.com>
2022-07-01 20:30:07 +0530

    Remove JS

--- src/test/java/automationAPI/JavascriptDemo.java
+++ src/test/java/automationAPI/JavascriptDemo.java
@@ -2,14 +2,5 @@ package automationAPI;
 
 public class JavascriptDemo {
 	
-	WebElement driver;
-	public static void main(String[] args) {
-		
-		
-	}
-	
-	public static void drawBorder(WebElement driver) {
-		
-		
-	}
+
 }
